cc487e68541633ced76988489233bbd3c655bfbc
@Apollo013 <paul.j.millar@gmail.com>
2023-10-09 14:06:05 +0100

    Adding tests for different ways to assert errors

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NodeTesting_Jest
+
+Demonstrates Node/Typescript testing with Jest.
+
+AAA Principles (Arrange, Act, Assert)
+F.I.R.S.T Principles (Fast, Independent, Repeatable, Self-Validating, Thorough)
+Jest Hooks
+Jest Aliases
+Code Coverage
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { getStringInfo, toUpperCase } from "../app/Utils";
+import { StringUtils, getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
     it("should return upper case", () => {
@@ -50,5 +50,49 @@ describe("getStringInfo for arg My-String should", () => {
     it("return right extra info", () => {
         const actual = getStringInfo("My-String");
         expect(actual.extraInfo).toEqual({});
+    });
+});
+
+describe("StringUtils tests", () => {
+    let sut: StringUtils;
+
+    //
+    beforeEach(() => {
+        sut = new StringUtils();
+        console.log("Setup");
+    });
+
+    afterEach(() => {
+        console.log(" Tear down");
+    });
+
+    it("Should return correct upperCase", () => {
+        const actual = sut.toUpperCase("abc");
+        expect(actual).toBe("ABC");
+    });
+
+    it("Should throw error on invalid argument - function", () => {
+        function expectError() {
+            const actual = sut.toUpperCase("");
+        }
+        expect(expectError).toThrow();
+        expect(expectError).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - arrow function", () => {
+        expect(() => {
+            sut.toUpperCase("");
+        }).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - try catch block", (done) => {
+        try {
+            sut.toUpperCase("");
+            done("GetStringInfo should throw error for invalid arg!");
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error).toHaveProperty("message", "Invalid argument!");
+            done();
+        }
     });
 });
